eca490ef2764fdd245a8b649963e222fc53d02a7
renato.cantarino@squadra.com.br <renato.cantarino@squadra.com.br>
2020-05-24 23:33:09 -0300

    openTracing

--- cadastro/src/main/java/com/cantarino/ifood/cadastro/resources/RestauranteResource.java
+++ cadastro/src/main/java/com/cantarino/ifood/cadastro/resources/RestauranteResource.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -29,7 +30,12 @@ import com.cantarino.ifood.cadastro.infra.mappers.RestauranteMapper;
 import com.cantarino.ifood.cadastro.models.Restaurante;
 
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
@@ -37,6 +43,10 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes (MediaType.APPLICATION_JSON)
 @Tag(name =  "restaurantes")
+@RolesAllowed("proprietario")
+@SecurityScheme(securitySchemeName = "ifood-oauth", type = SecuritySchemeType.OAUTH2,
+                flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token")))
+@SecurityRequirement(name = "ifood-oauth")
 public class RestauranteResource {
 
 
--- cadastro/src/test/java/com/cantarino/ifood/cadastro/RestauranteTest.java
+++ cadastro/src/test/java/com/cantarino/ifood/cadastro/RestauranteTest.java
@@ -11,9 +11,9 @@ import com.github.database.rider.cdi.api.DBRider;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.cantarino.ifood.cadastro.util.TokenUtils;
 
 
-import static io.restassured.RestAssured.given;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
@@ -27,18 +27,35 @@ import io.restassured.specification.RequestSpecification;
 @QuarkusTestResource(CadastroTestLifecycleManager.class)
 public class RestauranteTest {
 
+    private String token;
+    @BeforeEach
+    public void gereToken() throws Exception {
+        token = TokenUtils.generateTokenString("/JWTProprietarioClaims.json", null);
+    }
+
+    private RequestSpecification given() {
+        return RestAssured.given()
+                .contentType(ContentType.JSON).header(new Header("Authorization", "Bearer " + token));
+    }
+
+
 
     @Test
     @DataSet("restaurantes-cenario-1.yml")
     public void testGetRestaurantes()
     {
+
+
         String resultado = given()
-                .when().get("/api/restaurantes")
-                .then()
-                .statusCode(Status.OK.getStatusCode())
-                .extract().asString();
-       // Approvals.verifyJson(resultado);
+                            .when().get("/api/restaurantes")
+                            .then()
+                            .statusCode(Status.OK.getStatusCode())
+                            .extract().asString();
+
+        Approvals.verifyJson(resultado);
 
     }
+
+
 
 }
